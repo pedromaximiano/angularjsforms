62a37eca1a8a0c954022be28e9f2da47512fd275
pedromaximiano <pedro.maximiano@gmail.com>
2014-11-18 19:18:44 +0000

    Adds two-way data-binding.

--- AngularForms/app/EmployeeForm/efController.js
+++ AngularForms/app/EmployeeForm/efController.js
@@ -1,1 +1,14 @@
-﻿
+﻿angularFormsApp.controller("efController",
+    function efController($scope, efService) {
+        $scope.employee = efService.employee;
+        $scope.departments = [
+            "Administration",
+            "Marketing",
+            "Finance",
+            "Engineering"
+        ];
+
+        $scope.submitForm = function() {
+
+        };
+    });
--- AngularForms/app/EmployeeForm/efService.js
+++ AngularForms/app/EmployeeForm/efService.js
@@ -1,1 +1,14 @@
-﻿
+﻿angularFormsApp.factory("efService",
+    function () {
+        return {
+            employee: {
+                fullName: "Milton Waddams",
+                notes: "The ideal employee",
+                department: "Administration",
+                perkCar: false,
+                perkStock: true,
+                perkSixWeeks: false,
+                payrollType: "none"
+            }
+        };
+    });
